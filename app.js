62385fcf549a04bb0505a907af831be3c4c3b9c4
Ivan Pradana <ivan.it-consultant@prudential.co.id>
2021-08-13 20:52:58 +0700

    Remove duplicate middleware declaration

--- app.js
+++ app.js
@@ -22,11 +22,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(express.json({limit: '20mb'}));
-app.use(express.urlencoded({limit: '20mb'}));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
