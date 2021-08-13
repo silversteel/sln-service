05443dc6f9173ca2cd0fd9813d631dd3020df494
Ivan Pradana <ivan.it-consultant@prudential.co.id>
2021-08-13 20:34:37 +0700

    Extend payload size limit

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.json({limit: '20mb'}));
+app.use(express.urlencoded({limit: '20mb'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
