5bcccabdc084667f2a393222523691e763a3701b
Ivan Pradana <ivan.it-consultant@prudential.co.id>
2021-08-17 11:52:18 +0700

    Fix query error

--- app/customer/model.js
+++ app/customer/model.js
@@ -2,7 +2,7 @@ const db = require('../../database');
 
 async function insert(customer_id, username, fullname, phone_number, email, id_number, gender, address, created_by) {
     try {
-        const result = await db.query('with primary_by_username AS (select customer_id from celine_customer where is_primary = true and username = $2) insert into celine.customer(customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary created_by) values($1, $2, $3, $4, $5, $6, $7, $8, (CASE WHEN EXISTS(select * from primary_by_username) THEN false ELSE true END), $9)', [customer_id, username, fullname, phone_number, email, id_number, gender, address, created_by]);
+        const result = await db.query('with primary_by_username AS (select customer_id from celine_customer where is_primary = true and username = $2) insert into celine.customer(customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, created_by) values($1, $2, $3, $4, $5, $6, $7, $8, (CASE WHEN EXISTS(select * from primary_by_username) THEN false ELSE true END), $9)', [customer_id, username, fullname, phone_number, email, id_number, gender, address, created_by]);
         return result;
     } catch (err) {
         console.log(err.stack);
@@ -12,7 +12,7 @@ async function insert(customer_id, username, fullname, phone_number, email, id_n
 
 async function update(customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, updated_by) {
     try {
-        const result = await db.query('with disable_all_secondary(update celine.customer set is_primary = false where username = $2 and $9 = true) update celine.customer set username = $2, fullname = $3, phone_number = $4, email = $5, id_number = $6, gender = $7, address = $8, is_primary = $9 updated_by = $10, updated = now() where customer_id = $1', [customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, updated_by]);
+        const result = await db.query('with disable_all_secondary(update celine.customer set is_primary = false where username = $2 and $9 = true) update celine.customer set username = $2, fullname = $3, phone_number = $4, email = $5, id_number = $6, gender = $7, address = $8, is_primary = $9, updated_by = $10, updated = now() where customer_id = $1', [customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, updated_by]);
         return result;
     } catch (err) {
         console.log(err.stack);
