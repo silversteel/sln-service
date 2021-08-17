bf2e5cd3e6769cca3bc07f142dd57a43c0c823de
Ivan Pradana <ivan.it-consultant@prudential.co.id>
2021-08-17 11:56:15 +0700

    Fix query typo

--- app/customer/model.js
+++ app/customer/model.js
@@ -12,7 +12,7 @@ async function insert(customer_id, username, fullname, phone_number, email, id_n
 
 async function update(customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, updated_by) {
     try {
-        const result = await db.query('with disable_all_secondary(update celine.customer set is_primary = false where username = $2 and $9 = true) update celine.customer set username = $2, fullname = $3, phone_number = $4, email = $5, id_number = $6, gender = $7, address = $8, is_primary = $9, updated_by = $10, updated = now() where customer_id = $1', [customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, updated_by]);
+        const result = await db.query('with disable_all_secondary AS (update celine.customer set is_primary = false where username = $2 and $9 = true) update celine.customer set username = $2, fullname = $3, phone_number = $4, email = $5, id_number = $6, gender = $7, address = $8, is_primary = $9, updated_by = $10, updated = now() where customer_id = $1', [customer_id, username, fullname, phone_number, email, id_number, gender, address, is_primary, updated_by]);
         return result;
     } catch (err) {
         console.log(err.stack);
