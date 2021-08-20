d69edba5ce9bb55a486b933e3fe3ffaad3057fb4
Ivan Pradana <ivan.it-consultant@prudential.co.id>
2021-08-21 02:12:49 +0700

    Console query

--- app/report/model.js
+++ app/report/model.js
@@ -14,8 +14,9 @@ async function findAll(type) {
             period = "year";
         }
         console.log(period);
-        let query = "with detail_order as ( select od.order_id, string_agg(od.service_name, ', ') as service_names from celine.order_detail od group by od.order_id ) select c.fullname as customer_name, c.phone_number as customer_phone_number, od.service_names, e.fullname as employee_name, o.* from celine.order o join celine.customer c on c.customer_id = o.customer_id join celine.employee e on e.employee_id = o.employee_id join detail_order od on od.order_id = o.order_id where o.created between date_trunc($1, now()) and now()";
-        const result = await db.query(query, [period]);
+        let query = "with detail_order as ( select od.order_id, string_agg(od.service_name, ', ') as service_names from celine.order_detail od group by od.order_id ) select c.fullname as customer_name, c.phone_number as customer_phone_number, od.service_names, e.fullname as employee_name, o.* from celine.order o join celine.customer c on c.customer_id = o.customer_id join celine.employee e on e.employee_id = o.employee_id join detail_order od on od.order_id = o.order_id where o.created between date_trunc('"+period+"', now()) and now()";
+        console.log(query);
+        const result = await db.query(query);
         return result;
     } catch (err) {
         console.log(err.stack);
