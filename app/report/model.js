e8c980768df5aff6354dbc31be3bc6851924223c
Ivan Pradana <ivan.it-consultant@prudential.co.id>
2021-08-21 02:10:25 +0700

    add log

--- app/report/model.js
+++ app/report/model.js
@@ -5,6 +5,7 @@ async function findAll(type) {
         let period = "day";
         if (type === "daily") {
             period = "day";
+            console.log('disini');
         } else if (type === "weekly") {
             period = "week";
         } else if (type === "monthly") {
@@ -12,6 +13,7 @@ async function findAll(type) {
         } else if (type === "yearly") {
             period = "year";
         }
+        console.log(period);
         let query = "with detail_order as ( select od.order_id, string_agg(od.service_name, ', ') as service_names from celine.order_detail od group by od.order_id ) select c.fullname as customer_name, c.phone_number as customer_phone_number, od.service_names, e.fullname as employee_name, o.* from celine.order o join celine.customer c on c.customer_id = o.customer_id join celine.employee e on e.employee_id = o.employee_id join detail_order od on od.order_id = o.order_id where o.created between date_trunc($1, now()) and now()";
         const result = await db.query(query, [period]);
         return result;
